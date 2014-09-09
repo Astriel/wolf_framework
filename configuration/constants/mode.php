a18990ebaf73306141aabf817a511d7900e5b3cb
Astriel <quentinlozach@gmail.com>
2014-09-09 17:03:14 +0200

    [Structure] - Creation of the index
    - Creation of the constants of PATH in the index file
    - Creation of the constants folder in the configuration folder
    - Creation of the devlopment mode in the mode.php in the configuration
    - URL Rewritting to be able to deal with action
    controller/action1/action2/action3 with the variable requets

---
+++ configuration/constants/mode.php
@@ -0,0 +1,13 @@
+<?php
+/*
+* You can define multiple modes for your application 
+* 1. development
+* 2. testing
+* 3. production
+*/
+
+define( 'APP_MODE', "development" );
+
+/* End of file mode.php */
+/* Location: ./core/constants/mode.php */
+?>
--- index.php
+++ index.php
@@ -1,4 +1,38 @@
 <?php
+/*
+*
+*
+*
+*
+*/
+
+// Defining PATH constants related to the project
+define( 'BASE_PATH', dirname( realpath(__FILE__) ) );
+define( 'CORE_PATH', BASE_PATH . '/core' );
+define( 'CONFIG_PATH', BASE_PATH . '/configuration' );
+define( 'APP_PATH', BASE_PATH . '/application' );
+
+// Include constants 
+include CONFIG_PATH . '/constants/mode.php';
+
+// Configuring error handler
+switch ( APP_MODE ) {
+
+    case "development" :
+    case "testing" :
+        error_reporting( E_ALL );
+        break;
+
+    case "production" :
+        error_reporting( 0 );
+        break;
+
+    default:
+        error_reporting( E_ALL );
+
+}
+
+// echo $_GET['request'];
 
 /* End of file index.php */
 /* Location: ./index.php */
