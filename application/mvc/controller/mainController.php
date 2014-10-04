f721bbd5550d0416f0251025a170bca80232551c
Astriel <quentinlozach@gmail.com>
2014-10-04 12:11:23 +0200

    [Singleton refact]
    - Loader now in Singleton.
    - Router now in Singleton.
    - loadController now coded.
    - loadMethod now coded.
    - Creation of the error file.
    - Creation of the debug file.
    - The maincontroller and the index methods are loaded on the first page
    of the project.

--- application/mvc/controller/mainController.php
+++ application/mvc/controller/mainController.php
@@ -6,19 +6,7 @@
 * @Version : 0.1
 * @Role : This Controller will be load on the index page
 */ 
-class mainController { 
-
- /*
-  * @Class mainController
-  * @Author : Quentin LOZACH 
-  * @Version : 0.1
-  * @Method : __construct : Constructor of the mainController called in the index page
-  */ 
-  function __construct( ) { 
-    // $this->loadView("test");
-
-    echo "mainController loaded";
-  } 
+class mainController extends Singleton { 
 
  /*
   * @Class mainController
@@ -26,8 +14,8 @@ class mainController {
   * @Version : 0.1
   * @Method : When you go on the root of the project : index page, this method will be automatically load in this controller
   */ 
-  public function index( ) {
-
+  public static function index( ) {
+      echo "<h1 style='color:blue'>mainController loaded</h1>";
   		echo "<h1 style='color:blue'>I'm the index method, in the mainController.</h1>";
   		echo "<h3 style='color:blue'>I should appear on the root of the project and nowhere else.</h3>";
 
--- core/router/router.php
+++ core/router/router.php
@@ -1,7 +1,4 @@
 <?php 
-
-include BASE_PATH . '/core/lib/singleton.php';
-
 /**
 * Route the page the user is requested : controller/view/param
 *
@@ -44,20 +41,6 @@ class Router extends Singleton {
   */
   private $error;
 
-  /**
-  * Constructor of the Router class
-  *
-  * @method __construct
-  * @access private
-  * @param  void
-  * @return void 
-  * @author Quentin LOZACH
-  * @version 0.1
-  */
-  final private function __construct( ) {
-    
-  }
-
  /**
   * Each time a page will be load, this method will be called each time
   *
@@ -73,74 +56,75 @@ class Router extends Singleton {
   */ 
   public function parseRequest( $request = FALSE ) { 
 
-  	// Load the Loader class
-	include BASE_PATH . '/core/loader/loader.php';
+    // Load the Loader class
+  	include BASE_PATH . '/core/loader/loader.php';
 
-	// Instanciation of the loader which will load every file on the project
-	$loader = new Loader();
-  		
-  	// Are we on the index page or note ?
-	switch ( $request ) {
+    // Are we on the index page or note ?
+  	switch ( $request ) {
 
-		// We are on the index page
-	    case FALSE :
+  		// We are on the index page
+  	    case FALSE :
 
-	        // Load the mainController, the index method, mainView 
-	    	$this->controller = "mainController";
-	    	$this->method = "index";
-	    	$this->params = NULL;
+  	      // Load the mainController, the index method, mainView 
+  	    	$this->controller = "mainController";
+  	    	$this->method = "index";
+  	    	$this->params = NULL;
 
-	    	$loader->loadController( $this->controller );
+  	    	Loader::getInstance()->loadController( $this->controller );
+          Loader::getInstance()->loadMethod( $this->method );
 
-	        break;
+  	    break;
 
-	    // We are on another page so load the Controller and the model with the same name 
-	    default:
+  	    // We are on another page so load the Controller and the model with the same name 
+  	    default:
 
-			// Load the mainController, mainView and mainModel 
-			$requestExploded = explode( "/", $request );
+  			// Load the mainController, mainView and mainModel 
+  			$requestExploded = explode( "/", $request );
 
-			// If the user is trying to load a file directly in the URL such as : .php, .html, .css, .js, .sql ...
-			$requestSecured = explode( ".", $request );
+  			// If the user is trying to load a file directly in the URL such as : .php, .html, .css, .js, .sql ...
+  			$requestSecured = explode( ".", $request );
 
-			// If there is only a controller with no method, raise an error with error() method
-			if( empty( $requestExploded[1] ) && !empty( $requestExploded[0] ) ) {
-				$this->error("empty-method");
-			}
-			// If there is a controller and a method in parameters in the URL 
-			else {
+  			// If there is only a controller with no method, raise an error with error() method
+  			if( empty( $requestExploded[1] ) && !empty( $requestExploded[0] ) ) {
+  				$this->error("empty-method");
+  			}
+  			// If there is a controller and a method in parameters in the URL 
+  			else {
 
-				// If requestSecured[1] is not empty the user tried to load a file in the URL
-				if( isset( $requestSecured[1] ) ) {
-					// The controller is the name of the 1st param but cleaned with the explode
-					$this->controller = $requestSecured[0];
-				}
-				else {
-					// The controller is the first parameter of the URL 
-					$this->controller = $requestExploded[0];
-				}
+  				// If requestSecured[1] is not empty the user tried to load a file in the URL
+  				if( isset( $requestSecured[1] ) ) {
+  					// The controller is the name of the 1st param but cleaned with the explode
+  					$this->controller = $requestSecured[0];
+  				}
+  				else {
+  					// The controller is the first parameter of the URL 
+  					$this->controller = $requestExploded[0];
+  				}
 
-				// The method is the second parameter of the URL 
-				$this->method = $requestExploded[1];
-			}
+  				// The method is the second parameter of the URL 
+  				$this->method = $requestExploded[1];
+  			}
 
-			// Getting parameters, so let's keep only useful informations
-			unset( $requestExploded[0] );
-			unset( $requestExploded[1] );
+  			// Getting parameters, so let's keep only useful informations
+  			unset( $requestExploded[0] );
+  			unset( $requestExploded[1] );
 
-			// Getting the rest of the parameters in the $_GET['request'] in the URL
-			if( !empty( $requestExploded[2] ) ) {
-				$this->params = $requestExploded;
-			}
-			else {
-				$this->params = NULL;
-			}
+  			// Getting the rest of the parameters in the $_GET['request'] in the URL
+  			if( !empty( $requestExploded[2] ) ) {
+  				$this->params = $requestExploded;
+  			}
+  			else {
+  				$this->params = NULL;
+  			}
 
-	}
+        // Load the controller
+        Loader::getInstance()->loadController( $this->controller );
+
+        // Load the method
+        Loader::getInstance()->loadMethod( $this->method );
+  	}
 
   } 
-
-
 
  /**
   * If there is a problem in the core we can know exactly where does the problem comes from
@@ -154,19 +138,19 @@ class Router extends Singleton {
   */ 
   public function debug( ) { 
 
-  	echo "<h1>Debugging the Router class :</h1>";
+    echo "<h1>Debugging the Router class :</h1>";
 
-  	echo "<h2>Current controller :</h2>";
-	var_dump( $this->controller );
+    echo "<h2>Current controller :</h2>";
+  	var_dump( $this->controller );
 
-	echo "<h2>Current method in the $this->controller Controller :</h2>";
-	var_dump( $this->method );
+  	echo "<h2>Current method in the $this->controller Controller :</h2>";
+  	var_dump( $this->method );
 
-	// If there is a param, we can print it to the user
-	if( !is_null( $this->params ) ) {
-		echo "<h2>Current params :</h2>";
-		var_dump( $this->params );
-	}
+  	// If there is a param, we can print it to the user
+  	if( !is_null( $this->params ) ) {
+  		echo "<h2>Current params :</h2>";
+  		var_dump( $this->params );
+  	}
 
   } 
 
@@ -174,31 +158,43 @@ class Router extends Singleton {
   * If there is an error, this method will handle it, such as 404 error for example
   * 
   * @method error
-  * @access private
-  * @param string $typeError the type of the error 
+  * @access public
+  * @param string $typeError the type of the error, array $infosError some params linked to the error
   * @return void
   * @author Quentin LOZACH 
   * @version 0.1
   */ 
-  private function error( $typeError ) { 
+  public function error( $typeError, $infosError ) { 
 
-  	// There is an error in the Router
-  	$this->error = TRUE;
+    	// There is an error in the Router
+    	$this->setError( TRUE );
 
-  	switch ( $typeError ) {
+    	switch ( $typeError ) {
 
-		// If there is no method in the requested page
-	    case "empty-method" :
-	    	echo "<h1 style='color:red'>No method in the URL.<h1>";
-	        break;
+  		// If there is no method in the requested page
+  	    case "empty-method" :
+  	    	echo "<h1 style='color:red'>No method in the URL.</h1>";
+  	        break;
 
-	    case "wrong-format" :
-	    	echo "<h1 style='color:red'>The requested URL has a wronf format.<h1>";
-	        break;
+  	    case "wrong-format" :
+  	    	echo "<h1 style='color:red'>The requested URL has a wronf format.</h1>";
+  	        break;
 
-	    default:
-	        echo "<h1 style='color:red'>Undefined error.<h1>";
-	}
+        case "wrong-controller" :
+          echo "<h1 style='color:red'>The controller does not exist</h1>";
+            break;
+
+        case "no-controller" :
+          echo "<h1 style='color:red'>No controller was loaded</h1>";
+            break;
+
+        case "no-method" :
+          echo "<h1 style='color:red'>No method was loaded</h1>";
+            break;
+
+  	    default :
+  	        echo "<h1 style='color:red'>Undefined error.</h1>";
+  	}
 
   } 
 
@@ -216,6 +212,36 @@ class Router extends Singleton {
   */ 
   public function getError( ) { 
   	return $this->error;
+  }
+
+  /**
+  * Return the controller attribute of the class
+  * 
+  * @method getController
+  * @access public
+  * @param void
+  * @return string $controller return the attribute of the classe Router
+  * @author : Quentin LOZACH 
+  * @version : 0.1
+  */ 
+  public function getController( ) { 
+    return $this->controller;
+  }
+
+  /* =================== SETTERS ========================= */
+
+  /**
+  * Set the error attribute of the class
+  * 
+  * @method setError
+  * @access public
+  * @param string $value new value of the paramater error
+  * @return void
+  * @author : Quentin LOZACH 
+  * @version : 0.1
+  */ 
+  public function setError( $value ) { 
+    $this->error = $value;
   }
 
 }
